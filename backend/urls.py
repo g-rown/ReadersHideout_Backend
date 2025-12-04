db8cd4608b70938e43f2be954f05894436392010
g-rown <regulles.student@ua.edu.ph>
2025-12-04 22:47:19 +0800

    fix view.py & urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -26,8 +26,6 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('', include('lib_management.urls_frontend')),
-
     path('api/', include('lib_management.urls')),
     path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
